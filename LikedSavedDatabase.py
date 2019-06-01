b82319d5c60eb5104e6c6ea2ae7833ae62b1aaef
Macoy Madson <macoymadson@gmail.com>
2019-06-01 12:32:05 -0700

    Updated LikedSavedDatabase to have uniqueness requirements

--- LikedSavedDatabase.py
+++ LikedSavedDatabase.py
@@ -2,14 +2,6 @@ import os
 import sqlite3
 import submission as Submissions
 
-class FavoritesCollection:
-    def __init__(self, name):
-        self.name = name
-        self.collection = []
-
-    def addFavorite(self, submission):
-        self.collection.append(submission)
-
 class LikedSavedDatabase:
     def __init__(self, databaseFilePath):
         isNewDatabase = not os.path.exists(databaseFilePath)
@@ -21,31 +13,36 @@ class LikedSavedDatabase:
     def __del__(self):
         self.save()
         self.dbConnection.close()
-        
+
     def initializeEmptyDatabase(self):
         cursor = self.dbConnection.cursor()
-        
-        cursor.execute("create table Submissions (id integer primary key, source text, title text, author text, subreddit text, subredditTitle text, body text, bodyUrl text, postUrl text)")
-        cursor.execute("create table Comments (id integer primary key, source text, title text, author text, subreddit text, subredditTitle text, body text, bodyUrl text, postUrl text)")
+
+        cursor.execute("create table Submissions (id integer primary key, source text, title text, author text, subreddit text, subredditTitle text, body text, bodyUrl text, postUrl text, unique(postUrl))")
+        cursor.execute("create table Comments (id integer primary key, source text, title text, author text, subreddit text, subredditTitle text, body text, bodyUrl text, postUrl text, unique(postUrl))")
         cursor.execute("create table Collections (id integer primary key, name text)")
-        cursor.execute("create table SubmissionsToCollections (submissionKey integer, collectionKey integer)")
-        
+        cursor.execute("create table SubmissionsToCollections (submissionKey integer, collectionKey integer, unique(submissionKey))")
+        # Note that filePath is local to the server output directory,
+        # not the root filesystem. Submission key doesn't have to be
+        # unique so that multiple files can be associated with the
+        # same submission (e.g. an album, or self-uploaded files)
+        cursor.execute("create table FilesToSubmissions (filePath text, submissionKey integer, unique(filePath))")
+
         self.save()
-        
+
     def save(self):
         self.dbConnection.commit()
 
     def addComment(self, submission):
         cursor = self.dbConnection.cursor()
 
-        cursor.execute("insert into Comments values (NULL,?,?,?,?,?,?,?,?)",
+        cursor.execute("insert or ignore into Comments values (NULL,?,?,?,?,?,?,?,?)",
                        submission.getAsList())
         self.save()
 
     def addSubmission(self, submission):
         cursor = self.dbConnection.cursor()
 
-        cursor.execute("insert into Submissions values (NULL,?,?,?,?,?,?,?,?)",
+        cursor.execute("insert or ignore into Submissions values (NULL,?,?,?,?,?,?,?,?)",
                        submission.getAsList())
         self.save()
 
@@ -56,39 +53,45 @@ class LikedSavedDatabase:
         for row in cursor.execute("select * from Submissions"):
             print('\t', row)
         print("done")
-    
-    def getSubmissionByTitle(self, submissionTitle):
+
+    def getSubmissionsByTitle(self, submissionTitle):
         cursor = self.dbConnection.cursor()
 
-        # cursor.execute("select * from favorites where name=?", (collectionName,))
         cursor.execute("select * from Submissions where title=?", (submissionTitle,))
         return cursor.fetchone()
-    
+
     def createCollection(self, collectionName):
         cursor = self.dbConnection.cursor()
         cursor.execute("insert into Collections values (NULL, ?)", (collectionName,))
         self.save()
-
-        # print("bunch of stuff: collections")
-        # for row in cursor.execute("select * from Collections"):
-            # print(row)
-        # print("done")
-        
         cursor.execute("select * from Collections where name=?", (collectionName,))
         return cursor.fetchone()
 
     def addSubmissionToCollection(self, submissionId, collectionId):
         cursor = self.dbConnection.cursor()
-        cursor.execute("insert into SubmissionsToCollections values (?,?)",
+        cursor.execute("insert or ignore into SubmissionsToCollections values (?,?)",
                        (submissionId, collectionId))
+        self.save()
+
+    def associateFileToSubmission(self, filePath, submissionId):
+        cursor = self.dbConnection.cursor()
+        cursor.execute("insert or ignore into FilesToSubmissions values (?,?)",
+                       (filePath, submissionId))
         self.save()
 
     def getAllSubmissionsInCollection(self, collectionId):
         cursor = self.dbConnection.cursor()
-        
+
         cursor.execute("select * from Submissions, SubmissionsToCollections where Submissions.id = SubmissionsToCollections.submissionKey and SubmissionsToCollections.collectionKey = ?", (collectionId,))
 
-        return cursor.fetchone()
+        return cursor.fetchall()
+
+    def getAllFiles(self):
+        cursor = self.dbConnection.cursor()
+
+        cursor.execute("select * from FilesToSubmissions")
+
+        return cursor.fetchall()
 
 def testDatabase():
     db = LikedSavedDatabase('test.db')
@@ -102,19 +105,31 @@ def testDatabase():
     testSubmission.bodyUrl = "bodyUrl"
     testSubmission.postUrl = "postUrl"
     db.addSubmission(testSubmission)
-    dbSubmission = db.getSubmissionByTitle("title")
+    dbSubmission = db.getSubmissionsByTitle("title")
     dbCollection = db.createCollection("myCollection")
-    print(dbSubmission)
+    print(dbSubmission[0])
     print(dbCollection)
     db.addSubmissionToCollection(dbSubmission[0], dbCollection[0])
     print(db.getAllSubmissionsInCollection(dbCollection[0]))
 
 def testOnRealSubmissions():
     submissions = Submissions.readCacheSubmissions("Reddit_SubmissionCache.bin")
-    db = LikedSavedDatabase('test.db')
+    db = LikedSavedDatabase('test_v6.db')
     for submission in submissions:
         db.addSubmission(submission)
     db.printSubmissions()
+    dbCollection = db.createCollection("myCollection")
+
+    for title in ["Test 1", "Test 2"]:
+        dbSubmission = db.getSubmissionsByTitle(title)
+        if not dbSubmission:
+            print("Couldn't find {}".format(title))
+        else:
+            db.addSubmissionToCollection(dbSubmission[0], dbCollection[0])
+            db.associateFileToSubmission("{}.png".format(title), dbSubmission[0])
+
+    print(db.getAllSubmissionsInCollection(dbCollection[0]))
+    print(db.getAllFiles())
 
 if __name__ == '__main__':
     #testDatabase()
